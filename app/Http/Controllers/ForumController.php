05800a943680dab2bbf2b2147a67759c75946620
ahnafi <devsulthon@gmail.com>
2025-05-17 18:41:57 +0700

    feat : donation and forum api cursorpaginate

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Forum;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\CursorPaginator;
 
 class ForumController extends Controller
 {
+    public function index(): CursorPaginator
+    {
+        return Forum::with(['user', 'forumCategory'])
+            ->withCount('likedByUsers')
+            ->latest()
+            ->cursorPaginate(16);
+    }
+
     public function like(Forum $forum): RedirectResponse
     {
         $user = auth()->user();
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\DonationController;
+use App\Http\Controllers\ForumController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", fn() => "hello world")->name("post");
+Route::get("donations", [DonationController::class, "index"])->name("donations.index");
+Route::get("forums", [ForumController::class, "index"])->name("forum.index");
